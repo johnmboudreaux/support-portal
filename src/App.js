12773167721300779859d1a734381119cfe63ea4
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-13 10:33:00 -0500

    Filter arrow is now changing onclick from up to down. Changed app component into a class and passed state down to authAdminPage to accomplish this. removed utils folder for scss variable and added shared folder into src to make more semantic sense for shared scss variables.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/header/header.component.jsx';
 import AuthAdminPage from './pages/authAdminPage/authAdminPage.component.jsx';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <AuthAdminPage />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      filterClosed: true
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      filterClosed: !this.state.filterClosed
+    })
+  }
+  
+  render() {    
+    return (
+      <div className="App">
+        <Header />
+        <AuthAdminPage handleClick={this.handleClick} filterState={this.state.filterClosed}/>
+      </div>
+    );
+  }
 }
 
 export default App;
