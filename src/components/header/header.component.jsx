8ce9c7db835f6dbe32e405c367f4177e3ca728a9
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-13 11:07:59 -0500

    Add dropdowns to header-nav as well as new category in header-nav
    Added dropdowns per comp and added new category (new authorization) and dropdowns to header items per comp. Moved some styles around in header to affect all dropdowns instead of just userNav.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { DropdownButton, Dropdown } from 'react-bootstrap';
-import { FaTachometerAlt, FaChartLine, FaCog, FaCogs, FaRegChartBar, FaShieldAlt, FaLock } from 'react-icons/fa';
+import { FaTachometerAlt, FaChartLine, FaCog, FaCogs, FaRegChartBar, FaShieldAlt, FaLock, FaRegAddressCard } from 'react-icons/fa';
 import './header.styles.scss';
 
 const Header = () => (
@@ -10,10 +10,35 @@ const Header = () => (
     </div>
     <div className="header-nav">
       <ul>
-        <li><FaTachometerAlt /><a href="#">Dashboard</a></li>
-        <li><FaChartLine /><a href="#">Financials</a></li>
-        <li><FaRegChartBar /><a href="#">Reports</a></li>
-        <li><FaCog /><a href="#">Administration</a></li>
+        <li><FaTachometerAlt />
+            <DropdownButton id="dropdown-basic-button" title="Dashboard">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+        </li>
+        <li><FaRegAddressCard /><a href="#">New Authorization</a></li>
+        <li><FaChartLine />
+          <DropdownButton id="dropdown-basic-button" title="Financials">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton>
+        </li>
+        <li><FaRegChartBar />
+            <DropdownButton id="dropdown-basic-button" title="Reports">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+        </li>
+        <li><FaCog />
+            <DropdownButton id="dropdown-basic-button" title="Administration">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+        </li>
       </ul>
       <div className="user-nav">
         <FaCogs />
