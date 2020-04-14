972ad512f75f3e0d972ba531fc73eb6a795a1997
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-14 10:24:19 -0500

    Add edit auth page and route
    Created editAuthPage and imported into app.js. insterted a new route for editAuth in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Header from './components/header/header.component.jsx';
 import AuthAdminPage from './pages/authAdminPage/authAdminPage.component.jsx';
+import EditAuthPage from './pages/editAuthPage/editAuthPage.component.jsx';
 
 import './App.css';
 
@@ -10,7 +11,8 @@ const App = () => {
       <div className="App">
         <Header />
         <Switch>
-          <Route exact path='/' component={AuthAdminPage} />
+          <Route exact path='/admin' component={AuthAdminPage} />
+          <Route exact path='/edit' component={EditAuthPage} />
         </Switch>
       </div>
     );
