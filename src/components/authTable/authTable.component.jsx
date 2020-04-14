3154cca6606bb9a0c300fbca1b9abbdc1c7458d1
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-14 15:03:15 -0500

    Change auth table into stateful class component
    I need a way to conditionally display either a search message or the populated table after a search so i added state.
    
    I may end up pulling the state out and lifting it up to authadminpage since form-input will need to trigger the search.

--- src/components/authTable/authTable.component.jsx
+++ src/components/authTable/authTable.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import CustomButton from '../customButton/customButton.component';
 import dataNodes from '../../data/authorization.json';
@@ -6,48 +6,59 @@ import { Table } from 'react-bootstrap';
 import { FaEdit } from 'react-icons/fa';
 import './authTable.styles.scss';
 
-const AuthTable = ({ history, match }) => {
+class AuthTable extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tablePopulated: false
+    }
+  }
 
-  return (
-  <div className="table-wrapper">
-    <Table className="container auth-table" striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th></th>
-          <th>Id</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>DOB</th>
-          <th>Job Number</th>
-          <th>Reason</th>
-          <th>Company</th>
-          <th>Status</th>
-          <th>Last Updated</th>
-          <th>Visit Date</th>
-          <th>Active</th>
-        </tr>
-      </thead>
-      {dataNodes.map(node => (
-        <tbody key={node.authorizationPatientId}>
+  render() {
+    return (
+    <div className="table-wrapper">
+    {this.state.tablePopulated ?
+      <Table className="container auth-table" striped bordered hover variant="dark">
+        <thead>
           <tr>
-            <td valign="center"><CustomButton><FaEdit /></CustomButton></td>
-            <td>{node.authorizationPatientId}</td>
-            <td>{node.firstName}</td>
-            <td>{node.lastname}</td>
-            <td>{node.dob}</td>
-            <td>{node.jobNumber}</td>
-            <td>{node.reason}</td>
-            <td>{node.company}</td>
-            <td>{node.status}</td>
-            <td>{node.lastUpdated}</td>
-            <td>{node.visitDate}</td>
-            <td>{node.active}</td>
+            <th></th>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>DOB</th>
+            <th>Job Number</th>
+            <th>Reason</th>
+            <th>Company</th>
+            <th>Status</th>
+            <th>Last Updated</th>
+            <th>Visit Date</th>
+            <th>Active</th>
           </tr>
-        </tbody>
-      ))}
-      </Table>
-{/* <div className="waiting-for-search container"><h3>Search for an authorization number...</h3></div> */}
+        </thead>
+        {dataNodes.map(node => (
+          <tbody key={node.authorizationPatientId}>
+            <tr>
+              <td valign="center"><CustomButton><FaEdit /></CustomButton></td>
+              <td>{node.authorizationPatientId}</td>
+              <td>{node.firstName}</td>
+              <td>{node.lastname}</td>
+              <td>{node.dob}</td>
+              <td>{node.jobNumber}</td>
+              <td>{node.reason}</td>
+              <td>{node.company}</td>
+              <td>{node.status}</td>
+              <td>{node.lastUpdated}</td>
+              <td>{node.visitDate}</td>
+              <td>{node.active}</td>
+            </tr>
+          </tbody>
+          ))
+        }
+        </Table>
+        : <div className="waiting-for-search container"><h3>Search for an authorization number...</h3></div>
+    }
   </div>
-)};
+  )};
+}
 
 export default withRouter(AuthTable);
