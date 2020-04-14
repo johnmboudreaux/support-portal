720efac30d154dbfe48bbbee70f2179c061586b2
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-14 14:25:51 -0500

    Add mock data and make table populate dynamicallly
    Finished mock json file and imported it into authTable. Removed hardcoded table and added functionality to poplate the
    table dynamically.

--- src/components/authTable/authTable.component.jsx
+++ src/components/authTable/authTable.component.jsx
@@ -1,128 +1,53 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import CustomButton from '../customButton/customButton.component';
+import dataNodes from '../../data/authorization.json';
 import { Table } from 'react-bootstrap';
 import { FaEdit } from 'react-icons/fa';
 import './authTable.styles.scss';
 
-const AuthTable = () => (
-  <div className="table-wrapper">
-  <Table className="container auth-table" striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th></th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>DOB</th>
-      <th>Job Number</th>
-      <th>Reason</th>
-      <th>Company</th>
-      <th>Status</th>
-      <th>Last Updated</th>
-      <th>Visit Date</th>
-      <th>Active</th>
-    </tr>
-  </thead>
-  {/* <tbody>
-    <tr>
-      <td valign="center"><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-    <tr>
-      <td><CustomButton><FaEdit /></CustomButton></td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>8/22/1979</td>
-      <td>678</td>
-      <td>Pre-Employment</td>
-      <td>Prime</td>
-      <td>Clinic - In Progress</td>
-      <td>9/24/2019 10:16:08 PM</td>
-      <td>9/24/2019</td>
-    </tr>
-  </tbody> */}
-</Table>
-<div className="waiting-for-search container"><h3>Search for an authorization number...</h3></div>
-</div>
-);
+const AuthTable = ({ history, match }) => {
 
-export default AuthTable;
+  return (
+  <div className="table-wrapper">
+    <Table className="container auth-table" striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>DOB</th>
+          <th>Job Number</th>
+          <th>Reason</th>
+          <th>Company</th>
+          <th>Status</th>
+          <th>Last Updated</th>
+          <th>Visit Date</th>
+          <th>Active</th>
+        </tr>
+      </thead>
+      {dataNodes.map(node => (
+        <tbody key={node.authorizationPatientId}>
+          <tr>
+            <td valign="center"><CustomButton><FaEdit /></CustomButton></td>
+            <td>{node.authorizationPatientId}</td>
+            <td>{node.firstName}</td>
+            <td>{node.lastname}</td>
+            <td>{node.dob}</td>
+            <td>{node.jobNumber}</td>
+            <td>{node.reason}</td>
+            <td>{node.company}</td>
+            <td>{node.status}</td>
+            <td>{node.lastUpdated}</td>
+            <td>{node.visitDate}</td>
+            <td>{node.active}</td>
+          </tr>
+        </tbody>
+      ))}
+      </Table>
+{/* <div className="waiting-for-search container"><h3>Search for an authorization number...</h3></div> */}
+  </div>
+)};
+
+export default withRouter(AuthTable);
