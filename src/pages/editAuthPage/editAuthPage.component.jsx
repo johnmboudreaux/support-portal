947b8e3a043eb6dda50e68a27d1ca814548f6fbc
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-14 22:07:07 -0500

    Build out and style editAdminPage
    Started building out and styling the page. Removed unused component directory.

--- src/pages/editAuthPage/editAuthPage.component.jsx
+++ src/pages/editAuthPage/editAuthPage.component.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+// import { Button, SplitButton, DropdownType, ButtonGroup, Dropdown } from 'react-bootstrap';
+// import { FaSearch } from 'react-icons/fa'
 
+import './editAuthPage.styles.scss';
 const EditAuthPage = () => (
-  <div>EditAuthPage</div>
+  <main className="container-fluid">
+    <div className="main-content">
+      <div className="edit-page-status-selector container">
+        <h4>Details for Authorization 310788</h4>
+        <div className="status-group">
+          <span>Status:</span>
+          <select className="select-css">
+            <option>Clinic - In Progress</option>
+            <option>Apples</option>
+            <option>Bananas</option>
+            <option>Grapes</option>
+            <option>Oranges</option>
+          </select>
+        </div>
+      </div>
+      <hr />
+    </div>
+  </main>
 );
 
 export default EditAuthPage;
