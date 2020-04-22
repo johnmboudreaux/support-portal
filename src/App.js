13e0db10d305cbbe3b95a4498034deb4f80b2faa
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-22 11:31:02 -0500

    Add project-admin-page
    This commit contains the following:
    Created a project admin page
    Set up a route in app.js routing to project admin.
    Added some hyml elements with bootstrap rows, cols, and inputs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Header from './components/header/header.component.jsx';
 import AuthAdminPage from './pages/authAdminPage/authAdminPage.component.jsx';
+import ProjectAdminPage from './pages/projectAdminPage/projectAdminPage.component.jsx';
 
 import './App.css';
 
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Switch>
           <Route exact path='/admin' component={AuthAdminPage} />
+          <Route path='/project' component={ProjectAdminPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/projectAdminPage/projectAdminPage.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import FormInput from '../../components/form-input/form-input.component';
+
+import './projectAdminPage.styles.scss';
+
+
+const ProjectAdminPage = () => {
+  return (
+    <div className="project-admin-page-wrapper container">
+      <div className="row">
+        <div className="col">Name*</div>
+        <div className="col">Number*</div>
+        <div className="col">Owning Company*</div>
+        <div className="col">Start Date*</div>
+        <div className="col">End Date</div>
+      </div>
+      <div className="row">
+        <div className="col"><FormInput /></div>
+        <div className="col"><FormInput /></div>
+        <div className="col"><FormInput /></div>
+        <div className="col"><FormInput /></div>
+        <div className="col"><FormInput /></div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectAdminPage;
