8df72157e1dca4969a5bf6a2d89a088886bbdcfc
johnmboudreaux <jhnbdrx@gmail.com>
2020-04-23 19:38:59 -0500

    Add content to requested services and Reviewwer comments
    Added hard coded placeholder text to the reviewwer comments and a hard coded ul to make sure styles look ok. Changed button colors in auth details and requested services
    to match comp.

--- src/components/servicesRequested/servicesRequested.component.jsx
+++ src/components/servicesRequested/servicesRequested.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../customButton/customButton.component';
+import { ListGroup } from 'react-bootstrap';
 import { FaEdit } from 'react-icons/fa';
 
 import './servicesRequested.styles.scss';
@@ -12,10 +13,19 @@ const ServicesRequested = () => (
           <h5 className="title">Services Requested</h5>
           <Button><FaEdit /></Button>
         </div>
+        <div className="requested-service-list">
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </div>
       </div>
       <div className="col-6">
         <h5>Reviewer Comments</h5>
-        <p></p>
+        <p className="reviewer-comments">Reviewer comments will go here, wherver they may be, whenver they may be at, here they will be, nowhere else ever in the world of ever</p>
       </div>
     </div>
   </div>
