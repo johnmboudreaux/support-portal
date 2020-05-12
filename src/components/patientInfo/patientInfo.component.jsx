a6e07993e18df3f4fdb18cf98df3fe0c82333d23
johnmboudreaux <jhnbdrx@gmail.com>
2020-05-12 10:32:54 -0500

    Restructure  authDetails components
    Added some bootstrap columns and rows to restructure patient info html. It is now displaying in a cleaner format.

--- src/components/patientInfo/patientInfo.component.jsx
+++ src/components/patientInfo/patientInfo.component.jsx
@@ -6,25 +6,36 @@ import './patientInfo.styles.scss';
 
 const PatientInfo = ({ patient }) => {
   return (
-    <div className="patient-info col-4">
-      <div className="patient-info-title">
+    <div className='patient-info col-4'>
+      <div className='patient-info-title'>
         <h5>Patient Info</h5>
       </div>
-      <div><strong>Name:</strong>{` ${patient.firstName} ${patient.lastName}`}</div>
-      <div><strong>SSN:</strong>{` ${patient.ssn}`}</div>
-      <div><strong>Date Of Birth:</strong> {`${patient.dob}`}</div>
-      <div><strong>Phone:</strong> {` ${patient.phone}`}</div>
+      <div className='row'>
+        <strong className='col-6'>Name:</strong>
+        <p className='col-6'>{` ${patient.firstName} ${patient.lastName}`}</p>
+      </div>
+      <div className='row'>
+        <strong className='col-6'>SSN:</strong>
+        <p className='col-6'>{` ${patient.ssn}`}</p>
+      </div>
+      <div className='row'>
+        <strong className='col-6'>Date Of Birth:</strong>{' '}
+        <p className='col-6'>{`${patient.dob}`}</p>
+      </div>
+      <div className='row'>
+        <strong className='col-6'>Phone:</strong>{' '}
+        <p className='col-6'>{` ${patient.phone}`}</p>
+      </div>
     </div>
   );
-}
-
+};
 
 PatientInfo.propTypes = {
-  patient: PropTypes.object
-}
+  patient: PropTypes.object,
+};
 
 const mapStateToProps = (state) => ({
-  patient: state.patient
+  patient: state.patient,
 });
 
 export default connect(mapStateToProps)(PatientInfo);
