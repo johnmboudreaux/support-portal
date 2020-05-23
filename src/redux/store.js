ac4dca0b7eb5c17a32e0bdb3ba6ffd5fdfa88ed0
johnmboudreaux <jhnbdrx@gmail.com>
2020-05-23 14:00:47 -0500

    Add thunk middleware
    Added thunk middleware in and created one in reason for visit acrtions. Added seceral action types into action types and moved test reason data intp public folder to maock
    the fetch call. Needed to update the reason for visit select value when changed and needed to populate it based off of an authorization search as well. It now loads when the
    component mounts so it is ready for a search or change reason action.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,23 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-import rootReducer from "./root-reducer";
+import rootReducer from './root-reducer';
 
 const middleWares = [];
 
-if (process.env.NODE_ENV === "development") {
-  middleWares.push(logger);
-}
+process.env.NODE_ENV === 'development'
+  ? middleWares.push(logger, ReduxThunk)
+  : middleWares.push(ReduxThunk);
 
 const configureStore = (initialState) => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     initialState,
     composeEnhancers(applyMiddleware(...middleWares))
   );
-} 
+};
 
 export default configureStore;
